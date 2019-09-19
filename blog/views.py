83846de0dec0d453530da6dce4496a165a922671
liukang618 <1294167759@qq.com>
2019-09-19 18:20:08 +0800

    Initial commit

---
+++ blog/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render,HttpResponse
+from blog import models
+import datetime
+# Create your views here. 一个函数对应一个视图
+def text(request): #request 是返回的内容
+    times = datetime.datetime.now()
+    data = {
+        'time' : times
+    }
+    return render(request,'test.html',data)
+    # return HttpResponse("<h1>ok</h1>")
+User_List = []
+def userinfo(request):
+    if request.method == "POST":
+        u = request.POST.get("username",None)
+        s = request.POST.get("sex",None)
+        e = request.POST.get("email",None)
+        models.User_List.objects.create(
+            username = u,
+            sex=s,
+            email=e
+        )
+    User_List = models.User_List.objects.all()
+    return render(request,'userinfo.html',{"User_List":User_List})
---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\LiuKang\OneDrive\文档\Project\Python\myblog\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
